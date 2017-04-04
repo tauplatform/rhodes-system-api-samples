84ef186b5e432a8bda7471dabffb46db0a5e3a4e
Dmitry Soldatenkov <dsoldatenkov@tau-technologies.com>
2017-04-04 20:29:00 +0300

    fix async upload and download tests

--- app/AsyncDownloadFile/controller.rb
+++ app/AsyncDownloadFile/controller.rb
@@ -5,32 +5,36 @@ class AsyncDownloadFileController < Rho::RhoController
   #GET /AsyncDownloadFile
   def index
 
-    @@file_name = File.join(Rho::RhoApplication::get_base_app_path(), 'test.jpg')
+    @@file_name = File.join(Rho::RhoApplication::userFolder, 'test.jpg')
+
+    if File.exist?(@@file_name)
+        File.delete(@@file_name)
+    end
 
     Rho::AsyncHttp.download_file(
-      :url => 'http://www.primped.com.au/wp-content/uploads/2013/02/BrigitteBardotBedheadHair.jpg',
+      :url => 'http://s3-us-west-2.amazonaws.com/tautests/system-api-samples/AsyncDownloadFile/Brigitte_Bardot.jpg',
       :filename => @@file_name,
       :headers => {},
       :callback => (url_for :action => :httpdownload_callback),
       :callback_param => "" )
-      
+
     render :action => :wait
   end
-  
+
   def get_res
-    @@file_name    
+    @@file_name
   end
 
   def get_error
     @@error_params
   end
-  
+
   def httpdownload_callback
     puts "httpdownload_callback: #{@params}"
 
     if @params['status'] != 'ok'
         @@error_params = @params
-        WebView.navigate ( url_for :action => :show_error )        
+        WebView.navigate ( url_for :action => :show_error )
     else
         WebView.navigate ( url_for :action => :show_result )
     end
@@ -44,7 +48,7 @@ class AsyncDownloadFileController < Rho::RhoController
   def show_error
     render :action => :error, :back => '/app'
   end
-    
+
   def cancel_httpcall
     puts "cancel_httpcall"
     Rho::AsyncHttp.cancel()# url_for( :action => :httpdownload_callback) )
@@ -52,5 +56,5 @@ class AsyncDownloadFileController < Rho::RhoController
     @@get_result  = 'Request was cancelled.'
     render :action => :index, :back => '/app'
   end
-  
+
 end
--- app/AsyncUploadFile/controller.rb
+++ app/AsyncUploadFile/controller.rb
@@ -5,44 +5,55 @@ class AsyncUploadFileController < Rho::RhoController
   #GET /AsyncUploadFile
   def index
 
-    @@file_name = File.join(Rho::RhoApplication::get_base_app_path(), 'test_upload.txt')
+    #@@file_name = File.join(Rho::RhoApplication::get_model_path('app','AsyncUploadFile'), 'test_upload.png')
+    @@file_name = File.join(Rho::RhoApplication::userFolder, 'test_upload.txt')
     unless File.exists?(@@file_name)
         write_data  = "this is rhodes test"
         f = File.new(@@file_name, "w")
         f.write(write_data)
-        f.close        
+        f.close
     end
-    
+
+    #options = {
+    #    :url => 'http://5.19.220.82:8081/upload',
+    #    :filename => @@file_name,
+    #    :headers => {},
+    #    :body => 'uploading file',
+    #    :fileContentType => 'image/png'
+    #}
+    #Rho::Network.uploadFile(options, (url_for :action => :httpupload_callback))
+
+
     Rho::AsyncHttp.upload_file(
       #:url => 'http://dev.rhosync.rhohub.com/apps/SystemApiSamples/sources/client_log?client_id=19bdcf15-aca2-4e5a-9676-3c297c09bb11&device_pin=&log_name=',
-      :url => 'http://rhologs.herokuapp.com/client_log?client_id=&device_pin=&log_name=uptest',
+      :url => 'http://taulogservice.herokuapp.com/client_log?log_name=rhodes-system-api-samples',
       :filename => @@file_name,
       :headers => {},
       :callback => (url_for :action => :httpupload_callback),
       :callback_param => "" )
-      
+
     render :action => :wait
   end
-  
+
   def get_res
-    @@get_result    
+    @@get_result
   end
 
   def get_error
     @@error_params
   end
-  
+
   def httpupload_callback
     puts "httpupload_callback: #{@params}"
 
     if @params['status'] != 'ok'
         @@error_params = @params
-        WebView.navigate ( url_for :action => :show_error )        
+        WebView.navigate ( url_for :action => :show_error )
     else
         @@get_result = "Success. File: " + @@file_name;
         WebView.navigate ( url_for :action => :show_result )
     end
-        
+
   end
 
   def show_result
@@ -52,12 +63,12 @@ class AsyncUploadFileController < Rho::RhoController
   def show_error
     render :action => :error, :back => '/app'
   end
-    
+
   def cancel_httpcall
     Rho::AsyncHttp.cancel( url_for( :action => :httpupload_callback) )
 
     @@get_result  = 'Request was cancelled.'
     render :action => :index, :back => '/app'
   end
-  
+
 end
